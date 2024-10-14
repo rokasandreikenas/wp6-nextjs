7ddebea2a6a275cfc48eed3c805c0c4f7cef56c7
rokasandreikenas <shyptt@gmail.com>
2024-10-14 18:54:55 +0300

    Initial commit

---
+++ src/app/apie-mus/page.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function AboutUs() {
+  return (
+    <div>
+      <h1 className="heading">Sveiki iš apie mus</h1>
+      <Link href="/">Grįžti į pagrindinį puslapį</Link>
+    </div>
+  );
+}
---
+++ src/app/kontaktai/page.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Contacts() {
+  return (
+    <div>
+      <h1 className="heading">Sveiki iš kontaktų</h1>
+      <Link href="/">Grįžti į pagrindinį puslapį</Link>
+    </div>
+  );
+}
